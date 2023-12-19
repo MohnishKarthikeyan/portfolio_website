0981244c0310bba2ca9528e63128b80c9637fa0c
Mohnish Karthikeyan <mohnish1612@gmail.com>
2023-12-19 17:40:15 +0530

    Contact me form

--- script.js
+++ script.js
@@ -21,3 +21,23 @@ function openmenu() {
 function closemenu() {
   sidemenu.style.right = "-200px";
 }
+
+// google sheet contact me
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzTmn-mxc_GrD4QDShW0i6sg6rRh9utgFCfY9zHHJNZuomN6xop9nB1mERxnjXmpQ-G/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Your Message has been successfully sent!";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 6000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
